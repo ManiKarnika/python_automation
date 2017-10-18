fa24bd7c6379b88985eff3edb5017822c8995395
Dmitry Skvortsov <dscwortsov@gmail.com>
2017-10-18 06:02:58 -0700

    Задание №3: тест создания контакта

--- test_add_user.py
+++ test_add_user.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+from user import User
 
 
 def is_alert_present(wd):
@@ -18,43 +19,51 @@ class test_add_user(unittest.TestCase):
     def test_test_add_user(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_user_page(wd)
-        self.init_users_creation(wd)
+        self.init_users_creation(wd, User (name="name", last_name="last_name", address="address", phone="+15031234567", e_mail="sc@sc.com"))
+        self.logout(wd)
+
+    def test_test_add_empty_user(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_user_page(wd)
+        self.init_users_creation(wd, User (name="", last_name="", address="", phone="", e_mail=""))
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def init_users_creation(self, wd):
+    def init_users_creation(self, wd, user):
         #init users creation
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("name")
+        wd.find_element_by_name("firstname").send_keys(user.name)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("last_name")
+        wd.find_element_by_name("lastname").send_keys(user.last_name)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("address")
+        wd.find_element_by_name("address").send_keys(user.address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("+15031234567")
+        wd.find_element_by_name("home").send_keys(user.phone)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("sc@sc.com")
+        wd.find_element_by_name("email").send_keys(user.e_mail)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def open_user_page(self, wd):
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_home_page(self, wd):
---
+++ user.py
@@ -0,0 +1,9 @@
+class User:
+
+    def __init__(self, name, last_name, address, phone, e_mail):
+        self.name = name
+        self.last_name = last_name
+        self.address = address
+        self.phone = phone
+        self.e_mail = e_mail
+
