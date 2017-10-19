0065bdec56738e266ff9cd2398844603205120b5
Dmitry Skvortsov <dscwortsov@gmail.com>
2017-10-19 05:24:45 -0700

    оптимизация основного теста вынесением открытия страниц в функции

--- test_add_group.py
+++ test_add_group.py
@@ -17,20 +17,16 @@ class test_add_group(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
         self.init_groups_creation(wd, Group (name="group_name", header="group_header", footer="group_footer"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
         self.init_groups_creation(wd, Group (name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
@@ -40,6 +36,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
     def init_groups_creation(self, wd, group):
+        self.open_home_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
@@ -52,11 +49,13 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
     def open_group_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
