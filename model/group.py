3df50ebb1e012fdaad32131ab2e6d7da31414602
Dmitry Skvortsov <dscwortsov@gmail.com>
2017-11-26 14:48:27 -0800

    параметризация создания групп

--- model/group.py
+++ model/group.py
@@ -11,7 +11,7 @@ class Group:
 
     # functions' redefine
     def __repr__(self):  #  to format output in console log
-        return "%s:%s" % (self.id, self.name)
+        return "%s:%s:%s:%s" % (self.id, self.name, self.header, self.footer)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
